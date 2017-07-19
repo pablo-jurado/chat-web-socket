a6e350339eb79a62c6c4c013db3e7293fe653dc3
Pablo Jurado <pjurado@overnitecbt.com>
2017-07-19 08:51:00 -0500

    add public folder and split files

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+var path = require('path');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3100;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
