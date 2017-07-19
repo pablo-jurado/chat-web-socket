744154849a94ed8d6456b2b07fc2b4c4595667c3
Pablo Jurado <pjurado@overnitecbt.com>
2017-07-19 10:25:30 -0500

    create user tracking on server

--- index.js
+++ index.js
@@ -5,15 +5,35 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3100;
 
+var users = [];
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+// make css and js available
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  // console.log('a user connected');
+
+  socket.on('setUsername', function(data){
+    // check if user name already exist
+
+    if (users.indexOf(data) === -1){
+      users.push(data);
+      socket.emit('userSet', {username: data, users: users});
+    } else {
+      socket.emit('userExists', data + ' username is taken! Try some other username.');
+    }
+  })
+
+  socket.on('chat message', function(data){
+    io.emit('chat message', data);
+  });
+
+  socket.on('disconnect', function(){
+    // console.log('user disconnected');
   });
 });
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,44 @@
 $(function () {
   var socket = io();
-  $('form').submit(function(){
-    socket.emit('chat message', $('#m').val());
-    $('#m').val('');
-    return false;
+  // console.log(socket)
+  let currentUser = ''
+  // send chat message
+  $('#register').submit(function(e) {
+    e.preventDefault();
+    let user = $('#user').val();
+    setUsername(user)
   });
-  socket.on('chat message', function(msg){
-    $('#messages').append($('<li>').text(msg));
+
+  function setUsername (user) {
+    socket.emit('setUsername', user);
+  };
+
+  socket.on('userSet', function(data) {
+    currentUser = data.username
+    $('#feedback').html('welcome ' + currentUser)
+  });
+
+  socket.on('userExists', function(data) {
+    $('#feedback').html(data)
+  });
+
+  // send chat message
+  $('#chat').submit(function(e){
+    e.preventDefault();
+    if (currentUser !== '') {
+      let = message = $('#m').val()
+      if (message !== '') {
+        socket.emit('chat message', {user: currentUser, message: message});
+      }
+    }
+  });
+
+  // gets chat message
+  socket.on('chat message', function(data) {
+    let message = data.message
+    let user = data.user
+    $('#messages').append('<li><strong>' + user + ':</strong> ' + message);
     window.scrollTo(0, document.body.scrollHeight);
   });
+
 });
